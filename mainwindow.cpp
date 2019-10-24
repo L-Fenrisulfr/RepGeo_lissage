c29adc3030f161fabddc3b1111fad4f52cf6389b
Arnaud <arnaudconver7@gmail.com>
2019-10-24 15:13:57 +0200

    function barycentric Area

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,54 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+float MainWindow::barycentriqueArea(VertexHandle v, MyMesh *_mesh)
+{
+
+    QVector <int> neigborList;
+    QVector<float> produitVect;
+    float area;
+
+    for(MyMesh::VertexFaceIter vf_iter = _mesh->vf_iter(v); vf_iter.is_valid(); vf_iter ++)
+    {
+        FaceHandle vf = *vf_iter;
+        for(MyMesh::FaceVertexIter fv_iter = _mesh->fv_iter(vf); fv_iter.is_valid(); fv_iter ++)
+        {
+            VertexHandle vh2 = *fv_iter;
+            if(v != vh2)
+            {
+                //créer les 2 vecteur de la face et déterminer 1/2 aire du quad corrspondant
+                //edge of Face
+                neigborList.push_back(v.idx());
+                neigborList.push_back(vh2.idx());
+                qDebug()<<v.idx()<<vh2.idx();
+            }
+            //on récupère les 2 vecteurs U et V de la faces courante
+
+            //vectorU
+            float xu = _mesh->point(_mesh->vertex_handle(0))[0]; - _mesh->point(_mesh->vertex_handle(1))[0];
+            float yu = _mesh->point(_mesh->vertex_handle(0))[1] - _mesh->point(_mesh->vertex_handle(1))[1];
+            float zu = _mesh->point(_mesh->vertex_handle(0))[2] - _mesh->point(_mesh->vertex_handle(1))[2];
+            //vectorV
+            float xv = _mesh->point(_mesh->vertex_handle(2))[0] - _mesh->point(_mesh->vertex_handle(3))[0];
+            float yv = _mesh->point(_mesh->vertex_handle(2))[1] - _mesh->point(_mesh->vertex_handle(3))[1];
+            float zv = _mesh->point(_mesh->vertex_handle(2))[2] - _mesh->point(_mesh->vertex_handle(3))[2];
+
+            //produit vect U et V <=> à l'air du quad U*V
+            produitVect[0] = abs(yu*zv -  zu*yv);
+            produitVect[1] = abs(xu*zv - zu*xv);
+            produitVect[2] = abs(xu*yv - yu*xv);
+
+            //aire de la face courante
+            area += 0.5*sqrt(produitVect[0]*produitVect[0]+produitVect[1]*produitVect[1]+produitVect[2]*produitVect[2]);
+
+        }
+    }
+    return area/3.0;
+
+
+}
+
 void MainWindow::on_pushButton_clicked()
 {
     // Cet exemple montre comment afficher un cube (sans passer par une structure de maillage) directement dans un MeshViewerWidget
--- mainwindow.h
+++ mainwindow.h
@@ -22,10 +22,10 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    float MainWindow::barycentriqueArea(VertexHandle v, MyMesh *_mesh);
 
 private slots:
     void on_pushButton_clicked();
-
     void on_pushButton_2_clicked();
 
 private:
